736613bba771f8f3260f800ca74be78ce5763b3e
PCPetkov18 <PCPetkov18@codingburgas.bg>
2022-11-30 15:39:21 +0200

    Initial commit

---
+++ src/main/java/com/example/playerproject/Athlete.java
@@ -0,0 +1,2 @@
+package com.example.playerproject;public class Athlete {
+}
---
+++ src/main/java/com/example/playerproject/PlayerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.playerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerProjectApplication.class, args);
+	}
+
+}
